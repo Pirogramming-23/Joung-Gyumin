8f9343a00c479805c3bf9203233cb534841c8d45
gyubuk <g1370200@gmail.com>
2025-07-04 14:52:25 +0900

    5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -15,3 +15,19 @@ while True:
 for i in range(1, count+1):
     num = num+1
     print(f"playerA : {num}")
+
+while True:
+    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+    if not count.isdigit():
+        print("정수를 입력하세요")
+    else:
+        if count == "1" or count == "2" or count == "3":
+            count = int(count)
+            break
+        else:
+            print("1,2,3 중 하나를 입력하세요")
+
+for i in range(1, count+1):
+    num = num+1
+    print(f"playerB : {num}")

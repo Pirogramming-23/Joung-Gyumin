ce09eedc2e6fddb0a12e7aa499aacdbe13adcdb9
gyubuk <g1370200@gmail.com>
2025-07-04 15:30:12 +0900

    6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,33 +1,37 @@
 num = 0
 
-while True:
-    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-
-    if not count.isdigit():
-        print("정수를 입력하세요")
-    else:
-        if count == "1" or count == "2" or count == "3":
+while num < 31:
+    # Player A 입력
+    while True:
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+        if not count.isdigit():
+            print("정수를 입력하세요")
+        elif count == "1" or count == "2" or count == "3":
             count = int(count)
             break
         else:
             print("1,2,3 중 하나를 입력하세요")
 
-for i in range(1, count+1):
-    num = num+1
-    print(f"playerA : {num}")
-
-while True:
-    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
-
-    if not count.isdigit():
-        print("정수를 입력하세요")
-    else:
-        if count == "1" or count == "2" or count == "3":
+    for i in range(count):
+        num += 1
+        print(f"playerA : {num}")
+        if num == 31:
+            break
+    if num == 31:
+        break
+    
+    while True:
+        count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+        if not count.isdigit():
+            print("정수를 입력하세요")
+        elif count == "1" or count == "2" or count == "3":
             count = int(count)
             break
         else:
             print("1,2,3 중 하나를 입력하세요")
 
-for i in range(1, count+1):
-    num = num+1
-    print(f"playerB : {num}")
+    for i in range(count):
+        num += 1
+        print(f"playerB : {num}")
+        if num == 31:
+            break

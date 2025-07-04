82cf81fcffb4d13c0510bbf83c3c6b4f704686e3
gyubuk <g1370200@gmail.com>
2025-07-04 14:40:44 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,12 @@
 num = 0
-count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+while True:
+    count = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): ")
+
+    if not count.isdigit():
+        print("정수를 입력하세요")
+    else:
+        if count == "1" or count == "2" or count == "3":
+            break
+        else:
+            print("1,2,3 중 하나를 입력하세요")

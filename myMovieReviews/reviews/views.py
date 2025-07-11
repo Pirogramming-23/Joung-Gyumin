c045865b2974d8297b614839078d3f31e59d4218
gyubuk <g1370200@gmail.com>
2025-07-11 19:07:10 +0900

    잠시저장

--- myMovieReviews/reviews/views.py
+++ myMovieReviews/reviews/views.py
@@ -19,3 +19,21 @@ def review_create(request):
     else:
         form = ReviewForm()
     return render(request, 'reviews/review_form.html', {'form': form})
+
+def review_edit(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('review_detail', pk=review.pk)
+    else:
+        form = ReviewForm(instance=review)
+    return render(request, 'reviews/review_form.html', {'form': form})
+
+def review_delete(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == 'POST':
+        review.delete()
+        return redirect('review_list')
+    return render(request, 'reviews/review_confirm_delete.html', {'review': review})

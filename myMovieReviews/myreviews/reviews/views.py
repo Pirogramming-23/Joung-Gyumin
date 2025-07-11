1a5952da60c0a9c89beb050b0c016821697c739e
gyubuk <g1370200@gmail.com>
2025-07-11 17:53:06 +0900

    기능2구현

--- myMovieReviews/myreviews/reviews/views.py
+++ myMovieReviews/myreviews/reviews/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,  get_object_or_404
 from .models import Review
 # Create your views here.
 def review_list(request):
     reviews = Review.objects.all()
     return render(request, 'reviews/review_list.html', {'reviews': reviews})
+
+def review_detail(request, pk): 
+    review = get_object_or_404(Review, pk=pk)
+    return render(request, 'reviews/review_detail.html', {'review': review})
